882006f9b3b39c0d623faf81d43fda2ea00404e4
Ahmad Al Khatib <ahmad.al_khatib@stud.htwk-leipzig.de>
2025-08-14 05:26:37 +0200

    Deploy: 12 Themen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "047ec68834dbb37321598d2ea280c984
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "ab0bc1c230373c23b846e3ed32903e52",
+"flutter_bootstrap.js": "9d555f65025134ecdd6c4b33a2ad2429",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
